3700f6d9c5e08962bdf15195949af9362a44db6c
ritikakate93 <ritikakate22@gmail.com>
2025-07-31 18:17:14 +0530

    some more code changes

--- utils/dbFile.js
+++ utils/dbFile.js
@@ -2,11 +2,10 @@ const mongoose = require("mongoose");
 
 
 const connectDB = async () => {
-  const MONGO_URI = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@nodetask.gihpvnv.mongodb.net/${process.env.db_name}?retryWrites=true&w=majority`;
 
   try {
-    // console.log(MONGO_URI)
-    const conn = await mongoose.connect(MONGO_URI, {});
+    // console.log(process.env.MONGO_URI)
+    const conn = await mongoose.connect(process.env.MONGO_URI, {});
     console.log(`MongoDB Connected`);
   } catch (error) {
     console.error(error.message);
